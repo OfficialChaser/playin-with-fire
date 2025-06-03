84ccab154780145f2733aaa6aaf641d9779b1f68
Chase Maciel <112979903+OfficialChaser@users.noreply.github.com>
2025-06-03 16:50:34 -0700

    Small updates

--- scripts/Managers/lightning_handler.gd
+++ scripts/Managers/lightning_handler.gd
@@ -3,18 +3,16 @@ extends Node2D
 const LIGHTNING = preload("res://scenes/Objects/lightning.tscn")
 
 @onready var fire_tiles: FireTiles = $"../FireTiles"
-@export var min_lightning : int = 1
-@export var max_lightning : int = 10
+@export var min_lightning : int = 15
+@export var max_lightning : int = 20
 func _ready() -> void:
 	var count : int = 0
 	# Spawn a random number of lightnings (1-5) can tweak this number
 	for i in randi_range(min_lightning,max_lightning):
 		count += 1
 		spawn_lightning()
-	print(count)
 func spawn_lightning():
 	var cells = get_empty_cells()
-
 
 	#create new lightning then move it to random cell, 
 	#shift for correct sprite and spawn fire
@@ -29,16 +27,18 @@ func spawn_lightning():
 	#await get_tree().create_timer(0.25).timeout
 	
 	fire_tiles.set_cell(coord, 0, Vector2i(0, 0))
+	
 	fire_tiles.fire_health[coord] = fire_tiles.starting_health
 	fire_tiles.add_fire_sprite(coord)
 	
-func get_empty_cells(_tiles = fire_tiles, min_tile = Vector2i(-20,-5), max_tile = Vector2i(19,10)) -> Array:
+func get_empty_cells(tiles = fire_tiles, min_tile = Vector2i(-15,-11), max_tile = Vector2i(14,10)) -> Array:
 	var possible_cells := []
 	
 	for x in range(min_tile.x, max_tile.x):
 		for y in range(min_tile.y, max_tile.y):
 			var coord = Vector2i(x, y)
-			possible_cells.append(coord)
+			if !tiles.get_cell_tile_data(coord) or !tiles.get_cell_tile_data(coord).get_custom_data("boundary"):
+				possible_cells.append(coord)
 	
 	#remove house + nearby (+- 3 tiles) from empty cells
 	var house_min = Vector2i(-4,-6)
