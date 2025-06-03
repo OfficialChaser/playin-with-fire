a30a44bc864758f040dd687db03396b79a467fce
dev-Cabbage <jcmack101@gmail.com>
2025-06-02 22:18:06 -0400

    Fixed lightning spawning

--- scripts/Managers/lightning_handler.gd
+++ scripts/Managers/lightning_handler.gd
@@ -3,25 +3,18 @@ extends Node2D
 const LIGHTNING = preload("res://scenes/Objects/lightning.tscn")
 
 @onready var fire_tiles: FireTiles = $"../FireTiles"
-@export var max_lightning : int = 5
+@export var min_lightning : int = 1
+@export var max_lightning : int = 10
 func _ready() -> void:
-	
+	var count : int = 0
 	# Spawn a random number of lightnings (1-5) can tweak this number
-	for i in randi() % max_lightning:
+	for i in randi_range(min_lightning,max_lightning):
+		count += 1
 		spawn_lightning()
-
+	print(count)
 func spawn_lightning():
 	var cells = get_empty_cells()
-	
-	#remove house + nearby (+- 3 tiles) from empty cells
-	var house_min = Vector2i(-4,-6)
-	var house_max = Vector2i(4,4)
-	
-	for x in range(house_min.x, house_max.x + 1):
-		for y in range(house_min.y, house_max.y + 1):
-				var house_coord = Vector2i(x, y)
-				if cells.has(house_coord):
-					cells.erase(house_coord)
+
 
 	#create new lightning then move it to random cell, 
 	#shift for correct sprite and spawn fire
@@ -40,12 +33,22 @@ func spawn_lightning():
 	fire_tiles.add_fire_sprite(coord)
 	
 func get_empty_cells(tiles = fire_tiles, min_tile = Vector2i(-20,-5), max_tile = Vector2i(19,10)) -> Array:
-	var empty_cells := []
-
+	var possible_cells := []
+	
 	for x in range(min_tile.x, max_tile.x):
 		for y in range(min_tile.y, max_tile.y):
 			var coord = Vector2i(x, y)
-			if tiles.get_cell_tile_data(coord) == null:
-				empty_cells.append(coord)
-
-	return empty_cells
+			possible_cells.append(coord)
+	
+	#remove house + nearby (+- 3 tiles) from empty cells
+	var house_min = Vector2i(-4,-6)
+	var house_max = Vector2i(4,4)
+	
+	for x in range(house_min.x, house_max.x + 1):
+		for y in range(house_min.y, house_max.y + 1):
+				var house_coord = Vector2i(x, y)
+				if possible_cells.has(house_coord):
+					possible_cells.erase(house_coord)
+	
+	
+	return possible_cells
