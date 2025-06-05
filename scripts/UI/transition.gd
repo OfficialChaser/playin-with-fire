2aa3dd678cbe9fd6ab073a932eb3b678be86daf8
gobbe <gobbe5700@gmail.com>
2025-06-05 03:03:13 +0200

    fixed large file issue

--- scripts/UI/transition.gd
+++ scripts/UI/transition.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 signal animation_finished
 
 func play(anim: String):
-	$ColorRect/AnimationPlayer.play(anim)
+	$sweep/AnimationPlayer.play(anim)
 	print("test")
 
 func _on_animation_player_animation_finished(_anim_name):
