6f190cd2ebebe3f2d3ef3946fc1c3a57cceaa661
dev-Cabbage <jcmack101@gmail.com>
2025-06-05 21:38:33 -0400

    Update bloody_stuff.gd

--- misc/rules/rule_scripts/bloody_stuff.gd
+++ misc/rules/rule_scripts/bloody_stuff.gd
@@ -4,8 +4,8 @@ func set_rule_config(level : int = 1):
 	GameManager.water_color = Color("703843")
 	GameManager.blood_enabled = true
 	if level == 2:
-		GameManager.max_hp *= 2
-		GameManager.player_health *= 2
+		GameManager.max_hp += 100
+		GameManager.player_health += 100
 
 func reset_rule_config():
 	GameManager.water_color = Color.BLUE
