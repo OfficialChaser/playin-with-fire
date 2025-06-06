367190ebebe1e5b498adaa45584ac11e4067e9b0
gobbe <gobbe5700@gmail.com>
2025-06-06 05:25:55 +0200

    gambler

--- misc/rules/rule_scripts/gambling_addict.gd
+++ misc/rules/rule_scripts/gambling_addict.gd
@@ -4,8 +4,9 @@ func set_rule_config(level : int = 1):
 	GameManager.roll_tmrw = false
 	GameManager.rerolls += 2
 	if level > 2:
-		GameManager.reroll_cost = 5
+		GameManager.player_health += randi_range(-50, 50)
+		#GameManager.reroll_cost = 5
 
 func reset_rule_config():
 	GameManager.roll_tmrw = true
-	GameManager.reroll_cost = 1
+	#GameManager.reroll_cost = 1
