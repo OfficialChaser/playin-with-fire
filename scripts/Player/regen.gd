ad7c607f050fdcddb9ea6a2813f1e3d71cd465a1
gobbe <gobbe5700@gmail.com>
2025-06-08 23:41:21 +0200

    Update regen.gd

--- scripts/Player/regen.gd
+++ scripts/Player/regen.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _process(delta: float) -> void:
-	if GameManager.regen_enabled: 
+	if GameManager.regen_enabled and GameManager.in_game:
 		GameManager.player_health += delta * GameManager.regen
 		print_debug(delta * GameManager.regen)
 		print_debug("regen test")	
